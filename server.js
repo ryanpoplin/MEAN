adb6335a449a037fe421bdfdf784c8e4bec64e74
Byrdann Fox <mobileupstate@gmail.com>
2014-11-27 14:57:43 -0500

    Updates...

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// do a console.log() test on the process object...
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('./config/express.js');
 var app = express();
 app.listen(3000);
