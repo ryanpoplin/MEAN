f44670ec76c6213de69d675a61df2cef97deb9d1
Byrdann Fox <mobileupstate@gmail.com>
2014-11-27 23:23:21 -0500

    Updates...

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,4 +1,10 @@
 exports.render = function(req, res) {
     "use strict";
-    res.send('ByrdannFox Solutions');
+    if (req.session.lastVisit) {
+        console.log(req.session.lastVisit);
+    }
+    req.session.lastVisit = new Date();
+    res.render('index', {
+        title: 'ByrdannFox'
+    });
 };
--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
-var express = require('express'),
+var config = require('./config'),
+    express = require('express'),
     morgan = require('morgan'),
     compress = require('compression'),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
+    session = require('express-session');
 module.exports = function() {
     "use strict";
     var app = express();
@@ -16,6 +18,14 @@ module.exports = function() {
     }));
     app.use(bodyParser.json());
     app.use(methodOverride());
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: config.sessionSecret
+    }));
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
     require('../app/routes/index.server.routes.js')(app);
+    app.use(express.static('./public'));
     return app;
 };
